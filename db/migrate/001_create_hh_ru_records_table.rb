1466e860807f4640a2f15eb51323dfca9d914313
Nikita Mohylov <D1353L@users.noreply.github.com>
2020-05-27 22:03:27 +0300

    Implement saving all vacancies to DB and clean up old by schedule (#4)

--- db/migrate/001_create_hh_ru_records_table.rb
+++ db/migrate/001_create_hh_ru_records_table.rb
@@ -2,6 +2,10 @@
 
 class CreateHhRuRecordsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :hh_ru_records, force: true
+    create_table :hh_ru_records, force: true do |t|
+      t.string :title
+      t.string :company_name
+      t.timestamps
+    end
   end
 end
